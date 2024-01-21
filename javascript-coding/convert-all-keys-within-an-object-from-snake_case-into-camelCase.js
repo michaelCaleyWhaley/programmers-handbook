7e4bb9700cc050c961b1457c51117b7eb00633d0
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2024-01-21 16:40:26 +0000

    convert all keys to camelCase

---
+++ javascript-coding/convert-all-keys-within-an-object-from-snake_case-into-camelCase.js
@@ -0,0 +1,39 @@
+// Convert all keys within an object from snake_case into camelCase
+
+function convertObjToCamelCase(obj) {
+  for (const key in obj) {
+    const seperatedNames = key.split("_").map((keyPartial, index) => {
+      if (index === 0) {
+        return keyPartial;
+      } else {
+        return `${keyPartial[0].toUpperCase()}${keyPartial.substring(1)}`;
+      }
+    });
+
+    const joinedName = seperatedNames.join("");
+
+    if (seperatedNames.length > 1) {
+      obj[joinedName] = obj[key];
+      delete obj[key];
+    }
+
+    if (typeof obj[joinedName] === "object") {
+      convertObjToCamelCase(obj[joinedName]);
+    }
+  }
+}
+
+const testObject = {
+  ps_games: {
+    metal_gear_solid: { play: 1 },
+    crash_bandicoot: { play: 1 },
+    soul_calibur: { play: 1 },
+    star_wars: {
+      play: 1,
+      characters: [{ name: "han solo", super_power: "blaster" }],
+    },
+  },
+};
+
+convertObjToCamelCase(testObject);
+console.log("testObject: ", testObject);
