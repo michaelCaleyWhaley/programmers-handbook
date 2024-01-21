0c26848c4fb239da1042beb425c49301fd087991
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2024-01-21 18:05:40 +0000

    implement tabs

--- user-interfaces/tabs/src/App.tsx
+++ user-interfaces/tabs/src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.scss";
+import classnames from "classnames";
+
+// Notes for improving after MVP
+// add state to local storage so position is remembered when closing reopening
+// 
+
+const tabableContent = [
+  { button: "btn1", text: "First tab" },
+  { button: "btn2", text: "Second tab" },
+  { button: "btn3", text: "Third tab" },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeTab, setActiveTab] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul className="btn-list">
+        {tabableContent.map(({ button }, index) => (
+          <li>
+            <button
+              className={classnames("btn-list__btn", {
+                "btn-active": index === activeTab,
+              })}
+              onClick={() => {
+                setActiveTab(index);
+              }}
+            >
+              {button}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <ul className="content-list">
+        {tabableContent.map(({ text }, index) => (
+          <li
+            className={classnames("content-list__content", {
+              active: index === activeTab,
+            })}
+          >
+            <p>{text}</p>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
