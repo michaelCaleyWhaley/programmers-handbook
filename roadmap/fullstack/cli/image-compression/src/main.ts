0fa069c62c56cd6ff8b89ee34f183d3d076c3d4b
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2024-11-01 18:30:57 +0000

    working image compression of png,jpeg,webp

--- roadmap/fullstack/cli/image-compression/src/main.ts
+++ roadmap/fullstack/cli/image-compression/src/main.ts
@@ -1,7 +1,24 @@
-function test(): number {
-  console.log("LOG: ");
+import sharp from "sharp";
 
-  return 1;
-}
+import { opendir } from "node:fs/promises";
+import path from "node:path";
 
-test();
+void (async function compressImages(): Promise<void> {
+  const dir = await opendir("./uncompressed-images");
+  for await (const dirent of dir) {
+    const sharpImg = sharp(
+      `${path.resolve("uncompressed-images")}/${dirent.name}`
+    );
+
+    const { format } = await sharpImg.metadata();
+
+    const compressedImg =
+      format === "png"
+        ? sharpImg[format as "png"]({ compressionLevel: 6 })
+        : sharpImg[format as "jpeg" | "webp"]({ quality: 50 });
+
+    await compressedImg.toFile(
+      `${path.resolve("compressed-images")}/${dirent.name}`
+    );
+  }
+})();
