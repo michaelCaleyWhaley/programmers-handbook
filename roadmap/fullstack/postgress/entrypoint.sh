cbe52b9b1466ab524366e3802b334503b62d9b87
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2024-11-15 17:47:37 +0000

    SQL basic commands

---
+++ roadmap/fullstack/postgress/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ls
+psql -U postgres -f ./mock-data-person.sql
--- roadmap/fullstack/postgress/example-commands.sql
+++ roadmap/fullstack/postgress/example-commands.sql
@@ -57,3 +57,39 @@ SELECT first_name, last_name FROM person ORDER BY last_name DESC;
 
 -- Remove duplicates from the results
 SELECT DISTINCT first_name FROM person;
+
+-- WHERE clause
+SELECT * FROM person WHERE gender = 'Female' AND (country_of_birth = 'Poland' OR country_of_birth = 'China');
+
+-- Comparison 
+-- PSQL is capable of comparing results. Simple example below
+SELECT 1 = 1;
+-- results in t which is equal to true
+SELECT 1 = 2;
+-- results in f which is equal to false
+
+-- Limit result numbers
+SELECT * FROM person LIMIT 10;
+
+-- Limit and offset
+SELECT * FROM person limit 10 OFFSET 10;
+
+-- IN to filter by array of values
+SELECT * FROM person WHERE country_of_birth IN ('China', 'Poland', 'Japan');
+
+-- BETWEEN filter by dates
+SELECT * FROM person WHERE country_of_birth IN ('China', 'Poland', 'Japan') AND date_of_birth BETWEEN '2000-01-01' AND '2026-01-01';
+
+-- LIKE allows you to search using pattern matching
+SELECT * FROM person WHERE email LIKE '%google%';
+-- pattern match underscore represents any single char
+SELECT * FROM person WHERE email LIKE '%google____';
+
+-- ILIKE case insensitive version of LIKE
+SELECT * FROM person WHERE gender ILIKE 'female';
+
+-- GROUP BY statement groups rows that have the same values into summary rows
+SELECT country_of_birth, COUNT(*) FROM person GROUP BY country_of_birth ORDER BY country_of_birth ASC;
+
+-- HAVING FILTER FOR GROUP BY
+SELECT country_of_birth, COUNT(*) FROM person GROUP BY country_of_birth HAVING COUNT(*) >= 40 ORDER BY country_of_birth ASC;
