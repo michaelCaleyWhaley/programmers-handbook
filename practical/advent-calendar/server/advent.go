f93cc5174e4b9ab8a71619a499387695522c7378
Michael Caley <kneedeepwater@hotmail.com>
2024-12-06 17:03:01 +0000

    adjust server to handle lamda

--- practical/advent-calendar/server/advent.go
+++ practical/advent-calendar/server/advent.go
@@ -3,16 +3,46 @@ package main
 import (
 	"advent/controllers"
 	"advent/initialisers"
+	"advent/middleware"
+	"context"
+	"log"
+	"os"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	initialisers.LoadEnvVars()
+var ginLambda *ginadapter.GinLambda
+var isLocal = os.Args[len(os.Args)-1] == "--local"
 
-	r := gin.Default()
+func routes(r *gin.Engine) {
+	r.Use(middleware.CrossOrigin)
+	r.GET("/test", controllers.TestController)
 	r.GET("/gh-oauth", controllers.GetGhAccessToken)
-	r.Run()
+}
 
-	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+func init() {
+	initialisers.LoadEnvVars()
+	log.Printf("Gin cold start")
+	r := gin.Default()
+	routes(r)
+
+	if isLocal {
+		r.Run("localhost:4000")
+		return
+	}
+	ginLambda = ginadapter.New(r)
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return ginLambda.ProxyWithContext(ctx, req)
+}
+
+func main() {
+	if isLocal {
+		return
+	}
+	lambda.Start(Handler)
 }
