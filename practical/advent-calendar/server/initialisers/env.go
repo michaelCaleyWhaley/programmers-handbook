30268dd9176f7d9a20e33391afcc55eea7768a16
Michael Caley <kneedeepwater@hotmail.com>
2024-12-06 18:52:53 +0000

    deploy gateway, update server for local/server config

--- practical/advent-calendar/server/initialisers/env.go
+++ practical/advent-calendar/server/initialisers/env.go
@@ -2,13 +2,18 @@ package initialisers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
+var isLocal = os.Args[len(os.Args)-1] == "--local"
+
 func LoadEnvVars() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if isLocal {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 }
